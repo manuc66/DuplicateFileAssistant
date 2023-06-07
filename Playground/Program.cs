789b71f443668a53849de37d806514734ace0610
manuc66 <manuc66@gmail.com>
2023-06-07 23:03:06 +0200

    add empty main entry point

---
+++ Playground/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Buffers;
+using System.IO.Enumeration;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security.AccessControl;
+using System.Text;
+using FileCompare;
+
+Console.WriteLine("Hello, World!");
+
+
